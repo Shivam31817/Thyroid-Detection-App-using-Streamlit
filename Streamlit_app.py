83d37cba0b4d896cd471838c48333ffb2d97afe8
Shivam Yadav <118054946+Shivam31817@users.noreply.github.com>
2025-03-11 18:05:26 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -203,7 +203,7 @@ def main():
         clear_button = st.button('Clear', key='clear_button')
         
     if detect_button:
-        with st.spinner("Making predictions..."):
+    with st.spinner("Making predictions..."):
         # Preprocess inputs
         inputs = preprocess_inputs(age, sex, on_thyroxine, query_on_thyroxine, on_antithyroid_meds, sick,
                                    pregnant, thyroid_surgery, I131_treatment, query_hypothyroid, query_hyperthyroid,
