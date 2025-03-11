3effd86f77e25ab8fc0ce2e737c6dfd343245df7
Shivam Yadav <118054946+Shivam31817@users.noreply.github.com>
2025-03-11 17:56:24 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -234,5 +234,6 @@ def main():
             # Clear all input fields
             st.experimental_rerun()  # Rerun the script to reset all inputs
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
+
